def1e543beaffa83bb4a034b642bc42e5a1627a3
JoaoAlissonTec <joaoalisson222005@gmail.com>
2026-03-01 17:13:34 -0300

    :sparkles: Transaction from Person added

---
+++ webApi/SistemaControleGastosResidenciaisAPI/SistemaControleGastosResidenciaisAPI/DTOs/TransactionFromPersonDTO.cs
@@ -0,0 +1,14 @@
+﻿using SistemaControleGastosResidenciaisAPI.Enums;
+using SistemaControleGastosResidenciaisAPI.Models;
+
+namespace SistemaControleGastosResidenciaisAPI.DTOs
+{
+    public class TransactionFromPersonDTO
+    {
+        public required Guid Id { get; set; }
+        public required string Description { get; set; }
+        public required decimal Amount { get; set; }
+        public required TransactionType Type { get; set; }
+        public required Category Category { get; set; }
+    }
+}
--- webApi/SistemaControleGastosResidenciaisAPI/SistemaControleGastosResidenciaisAPI/obj/Debug/net10.0/SistemaControleGastosResidenciaisAPI.AssemblyInfo.cs
+++ webApi/SistemaControleGastosResidenciaisAPI/SistemaControleGastosResidenciaisAPI/obj/Debug/net10.0/SistemaControleGastosResidenciaisAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaControleGastosResidenciaisAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d575fc1c9f59bb7ca554d6cfbe63f3cac1736adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578d1ceabcab132774869b4c72ac37596fab7c59")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaControleGastosResidenciaisAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaControleGastosResidenciaisAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
